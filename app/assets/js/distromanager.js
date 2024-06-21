eaf894e5a63db82a46f639a1f68e94b0d4ceefb1
SolLa <hyeonhaeju@gmail.com>
2024-06-21 23:52:45 +0900

    Merge branch 'master' of https://github.com/hyeonhaeju/FlowVillage

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,11 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+<<<<<<< HEAD
 exports.REMOTE_DISTRO_URL = 'http://fi1ya2dfn6jca3dha4d6f.mcv.kr:1786/Nebula/distribution.json'
+=======
+exports.REMOTE_DISTRO_URL = 'http://flowhost.kro.kr:1786/Nebula/distribution.json'
+>>>>>>> 0b02d74fb25b6d024e36fdcf867f3342dcb33090
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
